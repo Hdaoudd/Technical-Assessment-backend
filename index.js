6bde717f02355819245878435e1c488a10b44e53
Hadi Daoud <hadi.daoud@ampersandlb.com>
2024-04-03 22:08:09 +0300

    d

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const LoginUser = require("./routes/loginuser");
 // const SignupUser = require("./routes/signupuser");
 
@@ -17,18 +17,18 @@ app.use((req, res, next) => {
 // app.use("/api/user", LoginUser);
 // app.use("/api/user", SignupUser);
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://hadidaoud885:usnW4jC0w8vLFXdi@cluster0.fnkqn6y.mongodb.net/"
-//   )
-//   .then(() => {
-//     app.listen(4000, () => {
-//       console.log("connected to db & listining on port", 4000, "!");
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+mongoose
+  .connect(
+    "mongodb+srv://hadidaoud885:usnW4jC0w8vLFXdi@cluster0.fnkqn6y.mongodb.net/"
+  )
+  .then(() => {
+    app.listen(4000, () => {
+      console.log("connected to db & listining on port!");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 app.listen(4000, () => {
   console.log("connected to db & listining on port!");
 });
