f4ee937f04b02b319d7a0050492ee9e380319f44
Hadi Daoud <hadi.daoud@ampersandlb.com>
2024-04-03 15:09:04 +0300

    Your commit message

---
+++ Routes/signupuser.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const User = require("../models/usermodel");
+const JWT = require("jsonwebtoken");
+const router = express.Router();
+
+const createToken = (_id) => {
+  return JWT.sign({ _id }, process.env.SECRET, { expiresIn: "1d" });
+};
+
+const signupUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.signup(username, password);
+    const token = createToken(user._id);
+
+    res.status(200).json({ username, password, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+router.post("/signup", signupUser);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const LoginUser = require("./Routes/loginuser");
+const SignupUser = require("./Routes/signupuser");
+
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/user", LoginUser);
+app.use("/api/user", SignupUser);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listining on port", process.env.PORT, "!");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
