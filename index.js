75dc37a54b0694412fd682072231f4aadb3cacfe
Hadi Daoud <hadi.daoud@ampersandlb.com>
2024-04-04 01:59:57 +0300

    last push

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
 
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
 app.use("/api/users", userRoutes);
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(
+    "mongodb+srv://hadidaoud885:usnW4jC0w8vLFXdi@cluster0.fnkqn6y.mongodb.net/"
+  )
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log("connected to db & listining on port", process.env.PORT, "!");
+    app.listen(4000, () => {
+      console.log("connected to db & listining on port", 4000, "!");
     });
   })
   .catch((error) => {
