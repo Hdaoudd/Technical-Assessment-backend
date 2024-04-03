789fc385698b9251153fdf8070fb32abcadd5066
Hadi Daoud <hadi.daoud@ampersandlb.com>
2024-04-03 23:17:11 +0300

    d

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const LoginUser = require("./routes/loginuser");
-const SignupUser = require("./routes/signupuser");
+const LoginUser = require("../routes/loginuser");
+const SignupUser = require("../routes/signupuser");
 
 const app = express();
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(req.path, req.method);
+//   next();
+// });
 
 app.use("/api/user", LoginUser);
 app.use("/api/user", SignupUser);
--- models/usermodel.js
+++ models/usermodel.js
@@ -13,38 +13,38 @@ const userSchema = new Schema({
   },
 });
 
-// userSchema.statics.login = async function (username, password) {
-//   if (!username || !password) {
-//     throw Error("All fields must be fields");
-//   }
-//   const user = await this.findOne({ username });
-//   if (!user) {
-//     throw Error("Incorrect Username");
-//   }
+userSchema.statics.login = async function (username, password) {
+  if (!username || !password) {
+    throw Error("All fields must be fields");
+  }
+  const user = await this.findOne({ username });
+  if (!user) {
+    throw Error("Incorrect Username");
+  }
 
-//   const match = await bcrypt.compare(password, user.password);
-//   if (!match) {
-//     throw Error("Incorrect Password");
-//   }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect Password");
+  }
 
-//   return user;
-// };
-// userSchema.statics.signup = async function (username, password) {
-//   if (!username || !password) {
-//     throw Error("All fields must be fields");
-//   }
+  return user;
+};
+userSchema.statics.signup = async function (username, password) {
+  if (!username || !password) {
+    throw Error("All fields must be fields");
+  }
 
-//   const exists = await this.findOne({ username });
+  const exists = await this.findOne({ username });
 
-//   if (exists) {
-//     throw Error("Username already in use");
-//   }
-//   const salt = await bcrypt.genSalt(10);
-//   const hash = await bcrypt.hash(password, salt);
+  if (exists) {
+    throw Error("Username already in use");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
 
-//   const user = await this.create({ username, password: hash });
+  const user = await this.create({ username, password: hash });
 
-//   return user;
-// };
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
