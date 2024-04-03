28b0ddf4b3bdb9be3397c789f6ab1e44abbf2e1a
Hadi Daoud <hadi.daoud@ampersandlb.com>
2024-04-03 22:01:26 +0300

    d

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
-// const LoginUser = require("./routes/loginuser");
-// const SignupUser = require("./routes/signupuser");
+const mongoose = require("mongoose");
+const LoginUser = require("./routes/loginuser");
+const SignupUser = require("./routes/signupuser");
 
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 
-// app.use(cors());
-// app.use(express.json());
-// app.use((req, res, next) => {
-//   console.log(req.path, req.method);
-//   next();
-// });
-
-// app.use("/api/user", LoginUser);
-// app.use("/api/user", SignupUser);
-
-// mongoose
-//   .connect(
-//     "mongodb+srv://hadidaoud885:usnW4jC0w8vLFXdi@cluster0.fnkqn6y.mongodb.net/"
-//   )
-//   .then(() => {
-//     app.listen(4000, () => {
-//       console.log("connected to db & listining on port", 4000, "!");
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-app.listen(4000, () => {
-  console.log("connected to db & listining on port!");
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
+
+app.use("/api/user", LoginUser);
+app.use("/api/user", SignupUser);
+
+mongoose
+  .connect(
+    "mongodb+srv://hadidaoud885:usnW4jC0w8vLFXdi@cluster0.fnkqn6y.mongodb.net/"
+  )
+  .then(() => {
+    app.listen(4000, () => {
+      console.log("connected to db & listining on port", 4000, "!");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
